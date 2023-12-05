a0f4cb0a3092f7501ceba675712bccdf52270553
GokulaDSBA6162 <gvijayak@uncc.edu>
2023-12-04 22:01:29 -0500

    Made Change

---
+++ Project.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import pip
+
+APP_TITLE = "IRS INDIVIDUAL TAX FILING - 2020"
+APP_SUB_TITLE = "STATEWISE INCOME"
+
+def main():
+    st.set_page_config(APP_TITLE)
+    st.title(APP_TITLE)
+    st.caption(APP_SUB_TITLE)
+
+#Load IRS Data
+pip.main(["install", "openpyxl"])
+df = pd.read_csv('20incyall - 1024 - Project.csv')
+
+
+
+st.write(df.shape)
+st.write(df.head())
+st.write(df.columns)
+
+
+st.map(df,
+    latitude='Latitude',
+    longitude='Longitude'
+    )
+#if__name__== "__main__":
+#main()
