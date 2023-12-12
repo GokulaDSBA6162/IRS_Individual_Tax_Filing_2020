30aeb1b8992ff5a51c710f26fcc992f79d0fbd94
GokulaDSBA6162 <gvijayak@uncc.edu>
2023-12-12 02:40:12 -0500

    Final_Project Commit 1

--- Project.py
+++ Project.py
@@ -7,19 +7,29 @@ import folium
 from streamlit_folium import st_folium
 import altair as alt
 
+#set the required titles
+
 APP_TITLE = "IRS INDIVIDUAL TAX FILING - 2020"
 APP_SUB_TITLE = "STATEWISE/COUNTYWISE INCOME"
 
 st.set_page_config(APP_TITLE, layout="wide")
 st.title(APP_TITLE)
-st.caption(APP_SUB_TITLE)
+st.markdown(
+    "*This section presents the following facts based on the chosen State and/or County:*\n"
+)
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
-#Load IRS Data
-pip.main(["install", "openpyxl"])
-df = pd.read_csv('data/20incyall - 1024 - STProject.csv')
+# #Load IRS Data
+# pip.main(["install", "openpyxl"])
+# df = pd.read_csv('data/20incyall - 1024 - STProject.csv')
 
-df.State = df.State.astype('string')
+@st.cache_data  #
+def load_data(url):
+    df = pd.read_csv(url)
+    return df
+
+# Assuming you have a CSV file locally
+df = load_data('data/20incyall - 1024 - STProject.csv')
 
 #Create Sidebar for State and County Selection
 
@@ -35,7 +45,7 @@ if not county:
 else:
     df3 = df2[df2["County name"].isin(county)]
 
-# #Filter the data based on State and County
+#Filter the data based on State and County
 
 if not state and not county:
     filtered_df = df
@@ -50,99 +60,184 @@ elif county:
 else:
     filtered_df = df3[df3["StateName"].isin(state)&df3["County name"].isin(county)]
 
-# ranking_g= filtered_df.groupby(['State','County name'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum','Number of head of household returns':'sum', 'Total income in Amount':'sum'})
-# st.write(ranking_g)
-
+#Calculate the Tax Income facts
 single = filtered_df["Number of single returns"].sum()
 joint = filtered_df["Number of joint returns"].sum()
 hh = filtered_df["Number of head of household returns"].sum()
 total_income = int(filtered_df["Total income in Amount"].sum())
 total_returns = filtered_df["Total income in Number of returns"].sum()
-#percent_single = 
+percent_single =  ((filtered_df["Number of single returns"].sum() /
+                    filtered_df['Total income in Number of returns'].sum()) * 100)
+percent_j =  ((filtered_df["Number of joint returns"].sum() /
+                    filtered_df['Total income in Number of returns'].sum()) * 100)
+percent_h =  ((filtered_df["Number of head of household returns"].sum() /
+                    filtered_df['Total income in Number of returns'].sum()) * 100)
 
-
+#Display settings
 col1, col2, col3, col4, col5 = st.columns(5)
+
 with col1:
     st.write("Total Returns:")
-    st.write(f"US $ {total_returns:,}")
+    st.write(f"{total_returns:,}")
 with col2:
     st.write("Total Income:")
     st.write(f"US $ {total_income:,}")
 with col3:
     st.write("Single Returns:")
-    st.write(f"US $ {single:,}")
+    st.write(f"{single:,}")
+    st.write(f"{percent_single:,.2f}%")
 with col4:
     st.write("Joint Returns:")
-    st.write(f"US $ {joint:,}")
+    st.write(f"{joint:,}")
+    st.write(f"{percent_j:,.2f}%")
 with col5:
     st.write("HOH Returns:")
-    st.write(f"US $ {hh:,}")
+    st.write(f"{hh:,}")
+    st.write(f"{percent_j:,.2f}%")
 
-state_agg= filtered_df.groupby(['StateName', 'State'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum','Number of head of household returns':'sum', 'Total income in Amount':'sum'})
+tab1,tab2 = st.tabs(['STATEWISE TAX INCOME DATA', 'COMPARE INCOME TAX FACTS'])
+with tab1:
 
-map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
-choropleth = folium.Choropleth(
-    geo_data='data/us-state-boundaries.geojson',
-    data=state_agg,
-    columns=('State', 'Total income in Amount'),
-    key_on='feature.properties.stusab',
-    line_opacity=0.8,
-    highlight=True
-)
-choropleth.geojson.add_to(map)
+    #Code to display map
+    st.markdown(
+    "***In this view, users of this application can explore visualizations for the selected states to understand which states generate higher tax income. The map highlights the highest tax-paying states in darker colors and the least tax-paying states in lighter colors. The bar charts default to displaying the Top 10 and Bottom 10 states in terms of Individual Income Tax of 2020. When a multi-select state filter is applied, it shows the Top and Bottom states based on the selection.***\n"
+    )
 
-choropleth.geojson.add_child(
-    folium.features.GeoJsonTooltip(['name'],labels=False)
-)
+    state_agg= filtered_df.groupby(['StateName', 'State'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum','Number of head of household returns':'sum', 'Total income in Amount':'sum'})
 
-st_map = st_folium(map, width=700, height=450)
+    map = folium.Map(location=[38, -96.5], zoom_start=4, scrollWheelZoom=False, tiles='CartoDB positron')
+   
+    choropleth = folium.Choropleth(
+        geo_data='data/us-state-boundaries.geojson',
+        data=state_agg,
+        columns=('State', 'Total income in Amount'),
+        key_on='feature.properties.stusab',
+        line_opacity=0.8,
+        highlight=True
+    )
+    choropleth.geojson.add_to(map)
 
-fact_df= df.groupby(['StateName', 'State'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum','Number of head of household returns':'sum', 'Total income in Amount':'sum'})
+    choropleth.geojson.add_child(
+        folium.features.GeoJsonTooltip(['name'],labels=False)
+    )
 
-st.subheader("Top 10 Highest Tax Paying States")
-freq_chart = alt.Chart(fact_df).mark_bar().transform_filter(
-    alt.FieldGTEPredicate(field='Total income in Amount', gte= 370000000)
-        ).encode(
-            y=alt.Y('StateName',sort=alt.EncodingSortField('Total income in Amount', op='min', order='descending')),
-            x='Total income in Amount'
-        ).properties(
-    height=400,
-    width=800
-)
+    st_map = st_folium(map, width=1400, height=700)
+    
+    #Code to display chart of Top 10
 
-freq_text = freq_chart.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3
-).encode(
-        text=alt.Text('Total income in Amount',format='.0f')
+    fact_df= filtered_df.groupby(['StateName', 'State'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum','Number of head of household returns':'sum', 'Total income in Amount':'sum'})
+
+    st.subheader("Top 10 Highest Tax Paying States")
+    # Create a rank column based on total income
+    fact_df['Rank'] = fact_df['Total income in Amount'].rank(ascending=False)
+
+    # Filter the data to include only the top 10 ranks
+    top_10_df = fact_df[fact_df['Rank'] <= 10]
+
+    # Create the Altair chart
+    freq_chart = alt.Chart(top_10_df).mark_bar().encode(
+        y=alt.Y('StateName:N', title="State", sort=alt.EncodingSortField('Total income in Amount', op='min', order='descending')),
+        x=alt.X('Total income in Amount:Q', axis=alt.Axis(grid=False)),
     ).properties(
-    height=400,
-    width=800
-)
-freq_chart + freq_text
+        height=400,
+        width=800
+    )
 
-st.subheader("Lowest Tax Paying States")
-freq_chart = alt.Chart(fact_df).mark_bar().transform_filter(
-    alt.FieldLTEPredicate(field='Total income in Amount', lte= 45000000)
-        ).encode(
-            y=alt.Y('StateName',sort=alt.EncodingSortField('Total income in Amount', op='min', order='ascending')),
-            x='Total income in Amount'          
-        ).properties(
-    height=400,
-    width=700
-)
+    freq_text = freq_chart.mark_text(
+        align='left',
+        baseline='middle',
+        dx=3
+    ).encode(
+        text=alt.Text('Total income in Amount:Q', format='.0f')
+    ).properties(
+        height=400,
+        width=800
+    )
 
-freq_text = freq_chart.mark_text(
-    align='left',
-    baseline='middle',
-    dx=3
-).encode(
-    text=alt.Text('Total income in Amount',format='.0f')
-).properties(
-    height=400,
-    width=700
-)
-(freq_chart + freq_text)
+    # Display the chart
+    (freq_chart + freq_text)
 
+    #Code to display chart of lowest 10
+
+    st.subheader("Lowest Tax Paying States")
+    # Create a rank column based on total income
+    fact_df['Rank'] = fact_df['Total income in Amount'].rank(ascending=True)
+
+    # Filter the data to include only the bottom 10 ranks
+    bottom_10_df = fact_df[fact_df['Rank'] <= 10]
+
+    # Create the Altair chart
+    freq_chart = alt.Chart(bottom_10_df).mark_bar().encode(
+        y=alt.Y('StateName:N', title="State", sort=alt.EncodingSortField('Total income in Amount', op='min', order='ascending')),
+        x=alt.X('Total income in Amount:Q', axis=alt.Axis(grid=False)),
+    ).properties(
+        height=400,
+        width=800
+    )
+
+    freq_text = freq_chart.mark_text(
+        align='left',
+        baseline='middle',
+        dx=3
+    ).encode(
+        text=alt.Text('Total income in Amount:Q', format='.0f')
+    ).properties(
+        height=400,
+        width=800
+    )
+
+    # Display the chart
+    (freq_chart + freq_text)
+
+with tab2:
+    
+    tab2_state_agg= filtered_df.groupby(['StateName'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum',
+                                                                                      'Number of head of household returns':'sum', 
+                                                                                      'Total income in Amount':'sum',
+                                                                                      'Total income in Number of returns':'sum',
+                                                                                      'Number of  farm returns':'sum',
+                                                                                      'Total itemized deductions in Number of returns':'sum',
+                                                                                      'Residential energy tax credit in Number of returns':'sum'})
+    tab2_state_agg.rename(columns={'Residential energy tax credit in Number of returns': 'Residential energy tax credit'}, inplace=True)
+    tab2_state_agg2= filtered_df.groupby(['StateName', 'County name'], as_index=False).aggregate({'Number of single returns':'sum','Number of joint returns':'sum',
+                                                                                    'Number of head of household returns':'sum', 
+                                                                                    'Total income in Amount':'sum',
+                                                                                    'Total income in Number of returns':'sum',
+                                                                                    'Number of  farm returns':'sum',
+                                                                                    'Total itemized deductions in Number of returns':'sum',
+                                                                                    'Residential energy tax credit in Number of returns':'sum'})
+    tab2_state_agg2.rename(columns={'Residential energy tax credit in Number of returns': 'Residential energy tax credit'}, inplace=True)
+
+    
+    st.header("Select a State and/or County from the sidebar to compare the facts on the scatterplot.")
+
+    st.markdown(
+    "*This view empowers users to select and compare states, exploring various tax statistics. The comparison feature allows users to understand the standing of different states and counties based on the chosen tax factor. For instance, users interested in discovering which state received Residential Energy Tax Credits in 2022 can make that selection to gather relevant insights.*\n"
+    )
+
+    st.markdown(
+    "***Note: While all tax facts provide information about returns, only 'Total Income' reflects the corresponding dollar amounts.***\n"
+)   
+
+    x_val = st.selectbox("Pick your x-axis fact",tab2_state_agg2.columns.drop(['StateName','County name']).tolist())
+    y_val = st.selectbox("Pick your y-axis fact",tab2_state_agg2.columns.drop(['StateName','County name']).tolist())
+
+    if not county:
+        scatter = alt.Chart(tab2_state_agg, title=f"{x_val} and {y_val}").mark_point().encode(
+        alt.X(x_val,title=f'{x_val}'),
+        alt.Y(y_val,title=f'{y_val}'),
+        tooltip=['StateName','Total income in Amount',x_val, y_val], size = 'Total income in Amount').configure_mark(
+        opacity=0.5,
+        color='blue')
+        st.altair_chart(scatter, theme="streamlit", use_container_width=True)
+    else:
+        scatter = alt.Chart(tab2_state_agg2, title=f"{x_val} and {y_val}").mark_point().encode(
+        alt.X(x_val,title=f'{x_val}'),
+        alt.Y(y_val,title=f'{y_val}'),
+        tooltip=['StateName','County name','Total income in Amount',x_val, y_val], size = 'Total income in Amount').configure_mark(
+        opacity=0.5,
+        color='StateName')
+        st.altair_chart(scatter, theme="streamlit", use_container_width=True)
+
+    st.dataframe(tab2_state_agg2)
 
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# 11_20_PR_Test
-I'm learning how to PR today!!
+# dsba_5122_final_project
+
+
+# Streamlit App Link
+
+
+# Introduction:
+Analyzing statewise IRS individual tax returns data can provide useful insights and solutions for various issues. Here are some potential problems that can be addressed using IRS individual tax returns data:
+
+Cost of Living Analysis: Compare the average income and tax burden across states to help individuals understand the cost of living variations. This information can be crucial for decisions related to relocation or job opportunities.
+
+Tax-Friendly States: Identify and highlight states that are considered tax-friendly based on factors such as income tax rates, available credits, and deductions. Provide information to help individuals make informed decisions about where to reside.
+
+Understanding State Tax Policies: Educate individuals about the specific tax policies in their state, including any unique credits or deductions available. This information can aid in tax planning and optimizing financial decisions.
+
+Impact of State Policies on Take-Home Pay: Analyze the impact of state income tax rates on take-home pay. Provide insights into how different tax policies can affect disposable income.
+
+Comparative Analysis of States: Enable users to compare tax data among different states to make informed decisions about where to live or conduct business. Highlight states with lower tax burdens or favorable tax environments.
+
+Retirement Planning by State: Provide insights into how state tax policies can impact retirement planning. Analyze states with favorable conditions for retirees, considering factors such as pension taxation and Social Security benefits.
+
+Education on State Tax Credits: Educate individuals about state-specific tax credits and incentives. This could include credits for education, homeownership, energy efficiency, and other state-supported initiatives.
+
+Impact of Local Taxes: Consider the impact of local taxes (city or county) in addition to state taxes. Provide a comprehensive view of the overall tax burden at different geographical levels.
+
+State Economic Trends: Analyze statewise tax data to identify economic trends and patterns. This information can be useful for individuals considering job opportunities, investments, or business ventures.
+
+Decision Support for Entrepreneurs: For entrepreneurs and small business owners, analyze state tax data to provide insights into business-related taxes, credits, and deductions. Support decision-making related to business location and operations.
+
+# Data/Operation Abstraction Design:
+ For the purpose of this project, I have focused on a select set of data points as highlighted below. However, it's important to note that the dataset contains a wealth of additional information that can be leveraged to address a broader range of inquiries and objectives.
+
+# Future Work:
+
+Analyzing tax-related data geographically through maps can uncover regional variations and trends. In future improvements, I aim to enhance map interactivity, although my attempts in this regard were unsuccessful this time.
+
+Explore and integrate more variables from the dataset to gain valuable insights. This may encompass factors such as deductions, credits, or specific sources of income.
+
+Create visualizations illustrating trends over time. This involves scrutinizing alterations in income, deductions, or tax credits over various years or periods.
